1d78872cbb414609bb894e89e65fb11ff01ac1d6
pampyras <pampyras@gmail.com>
2019-07-31 10:38:07 +0300

    Update omniva.js

--- views/js/omniva.js
+++ views/js/omniva.js
@@ -61,7 +61,7 @@ var omniva_addrese_change = false;
             e.preventDefault();            
             showModal();
         });
-        $('.terminal-container').on('click','#show-omniva-map',function(e){
+        $('body').on('click','#show-omniva-map',function(e){
             e.preventDefault();            
             showModal();
         });
@@ -127,6 +127,7 @@ var omniva_addrese_change = false;
                     }
                 });
             }
+            console.log('map');
             $('#omnivaLtModal').show();
             //getLocation();
             var event;
